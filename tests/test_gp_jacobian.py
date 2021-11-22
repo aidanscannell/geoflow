c0f8de5db949666f17ae4d118f1b37b7ab6a86f7
Aidan Scannell <scannell.aidan@gmail.com>
2021-11-22 10:48:10 +0000

    commit without test written properly

--- tests/test_gp_jacobian.py
+++ tests/test_gp_jacobian.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
-from geoflow.gp import gp_predict_jacobian
-from gpflow.config import default_float
 
 # from absl.testing import parameterized
 from gpflow.kernels import SeparateIndependent, SquaredExponential
 from gpflow.likelihoods import Gaussian
+from gpflow.inducing_variables import InducingPoints
 from gpflow.mean_functions import Constant
-from gpflow.models import SVGP
+from gpflow import default_float
+from geoflow.gp import SVGP
+
+# from gpflow.models import SVGP
+from geoflow.manifolds import GPManifold
 
 rng = np.random.RandomState(1)
 
@@ -16,10 +19,9 @@ rng = np.random.RandomState(1)
 num_data = 10
 input_dim = 2
 output_dim = 1
-num_inducing = 3
+num_inducing = 30
 Xnew = tf.constant(rng.randn(num_data, input_dim), dtype=default_float())
 print(Xnew.shape)
-
 
 # SVGP variants
 if output_dim > 1:
@@ -31,7 +33,6 @@ if output_dim > 1:
 else:
     # kernel = SquaredExponential(lengthscales=np.ones(input_dim), variance=2.0)
     kernel = SquaredExponential()
-# inducing_variable = rng.randn(num_inducing, input_dim)
 inducing_variable = InducingPoints(rng.randn(num_inducing, input_dim))
 
 svgp = SVGP(
@@ -45,15 +46,10 @@ svgp = SVGP(
 )
 print(svgp.kernel.lengthscales)
 
-# gp_predict_jacobian(
-#     Xnew,
-#     svgp.inducing_variable.Z,
-#     svgp.kernel,
-#     svgp.mean_function,
-#     svgp.q_mu,
-#     full_cov=False,
-#     q_sqrt=svgp.q_sqrt,
-#     whiten=svgp.whiten,
-# )
+manifold = GPManifold(gp=svgp)
 
-svgp.predict_jacobian(Xnew)
+jac_mean, jac_cov = manifold.embed_jac(Xnew)
+print(jac_mean.shape)
+print(jac_cov.shape)
+print(jac_mean)
+print(jac_cov)
