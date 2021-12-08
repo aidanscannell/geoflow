7c3586accc5e0d7aee7bd51f1d85950e7dd47fc7
Aidan Scannell <scannell.aidan@gmail.com>
2021-12-08 13:11:03 +0000

    Added types for input/output data, JacMeanAndCovariance and NumInducing

--- geoflow/custom_types.py
+++ geoflow/custom_types.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 import typing
+from typing import Tuple
+
+import tensor_annotations.tensorflow as ttf
 from tensor_annotations import axes
 
 # Axes types
 NumData = typing.NewType("NumData", axes.Axis)
 InputDim = typing.NewType("InputDim", axes.Axis)
 OutputDim = typing.NewType("OutputDim", axes.Axis)
-# NumInducing = typing.NewType("NumInducing", axes.Axis)
+NumInducing = typing.NewType("NumInducing", axes.Axis)
 One = typing.NewType("One", axes.Axis)
+
+InputData = ttf.Tensor2[NumData, InputDim]
+OutputData = ttf.Tensor2[NumData, OutputDim]
+
+JacMeanAndVariance = Tuple[
+    ttf.Tensor3[NumData, InputDim, OutputDim],
+    ttf.Tensor4[NumData, OutputDim, InputDim, InputDim],
+]
